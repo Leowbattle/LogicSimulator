aac7d5b61f2fab3bf7d315ef0ae2933a00240079
Leo Battle <leowbattle@gmail.com>
2021-10-05 07:52:47 +0100

    Added camera panning

--- LogicSimulator/CircuitViewControl.cs
+++ LogicSimulator/CircuitViewControl.cs
@@ -17,9 +17,17 @@ namespace LogicSimulator
 		public CircuitViewControl()
 		{
 			InitializeComponent();
+
+			MouseDown += CircuitViewControl_MouseDown;
+			MouseUp += CircuitViewControl_MouseUp;
+			MouseMove += CircuitViewControl_MouseMove;
+			MouseWheel += CircuitViewControl_MouseWheel;
 		}
 
 		Font f = new Font(FontFamily.GenericMonospace, 10);
+
+		PointF camPos = new PointF(0, 0);
+		float scale = 5;
 
 		protected override void OnPaint(PaintEventArgs pe)
 		{
@@ -29,9 +37,41 @@ namespace LogicSimulator
 			g.SmoothingMode = SmoothingMode.AntiAlias;
 			g.TextRenderingHint = TextRenderingHint.AntiAlias;
 
-			g.ScaleTransform(15, 15);
+			g.ScaleTransform(scale, scale);
+			g.TranslateTransform(-camPos.X, -camPos.Y);
 
 			g.DrawString("Test", f, Brushes.Black, 0, 0);
+		}
+
+		bool mouseDown = false;
+
+		private void CircuitViewControl_MouseWheel(object sender, MouseEventArgs e)
+		{
+			
+		}
+
+		PointF lastMousePos = new PointF(0, 0);
+		private void CircuitViewControl_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (mouseDown)
+			{
+				camPos.X -= (e.X - lastMousePos.X) / scale;
+				camPos.Y -= (e.Y - lastMousePos.Y) / scale;
+
+				Invalidate();
+			}
+
+			lastMousePos = e.Location;
+		}
+
+		private void CircuitViewControl_MouseUp(object sender, MouseEventArgs e)
+		{
+			mouseDown = false;
+		}
+
+		private void CircuitViewControl_MouseDown(object sender, MouseEventArgs e)
+		{
+			mouseDown = true;
 		}
 	}
 }
