ffce3458f75e6392e1065873b60fbdceb2c5fdaa
Leo Battle <leowbattle@gmail.com>
2021-10-05 08:07:08 +0100

    Added camera zoom

--- LogicSimulator/CircuitViewControl.cs
+++ LogicSimulator/CircuitViewControl.cs
@@ -18,16 +18,19 @@ namespace LogicSimulator
 		{
 			InitializeComponent();
 
+			ResizeRedraw = true;
+			DoubleBuffered = true;
+
 			MouseDown += CircuitViewControl_MouseDown;
 			MouseUp += CircuitViewControl_MouseUp;
 			MouseMove += CircuitViewControl_MouseMove;
 			MouseWheel += CircuitViewControl_MouseWheel;
 		}
 
-		Font f = new Font(FontFamily.GenericMonospace, 10);
+		Font font = new Font(FontFamily.GenericMonospace, 10);
 
 		PointF camPos = new PointF(0, 0);
-		float scale = 5;
+		float scale = 1;
 
 		protected override void OnPaint(PaintEventArgs pe)
 		{
@@ -40,17 +43,31 @@ namespace LogicSimulator
 			g.ScaleTransform(scale, scale);
 			g.TranslateTransform(-camPos.X, -camPos.Y);
 
-			g.DrawString("Test", f, Brushes.Black, 0, 0);
+			g.DrawString("Test", font, Brushes.Black, 0, 0);
 		}
-
-		bool mouseDown = false;
 
 		private void CircuitViewControl_MouseWheel(object sender, MouseEventArgs e)
 		{
-			
+			float scaleChange = scale * e.Delta / 2000f;
+			float newScale = scale + scaleChange;
+
+			var oldWorldMouseX = e.X / scale + camPos.X;
+			var oldWorldMouseY = e.Y / scale + camPos.Y;
+
+			var worldMouseX = e.X / newScale + camPos.X;
+			var worldMouseY = e.Y / newScale + camPos.Y;
+
+			camPos.X -= worldMouseX - oldWorldMouseX;
+			camPos.Y -= worldMouseY - oldWorldMouseY;
+
+			scale = newScale;
+
+			Invalidate();
 		}
 
+		bool mouseDown = false;
 		PointF lastMousePos = new PointF(0, 0);
+
 		private void CircuitViewControl_MouseMove(object sender, MouseEventArgs e)
 		{
 			if (mouseDown)
